507f109b44c698f0a287c7e0efdbe5c418042586
itupagn <Noeline.PAGESY_ssii@p.fr>
2022-02-22 20:45:56 +0100

    init project

---
+++ yescapa/yescapa/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  yescapa
+//
+//  Created by PAGESY Noëline (i-BP - OPEN SAS) on 22/02/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ yescapa/yescapaUITests/yescapaUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  yescapaUITestsLaunchTests.swift
+//  yescapaUITests
+//
+//  Created by PAGESY Noëline (i-BP - OPEN SAS) on 22/02/2022.
+//
+
+import XCTest
+
+class yescapaUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
